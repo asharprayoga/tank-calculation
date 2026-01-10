b32c20dccf916e8346723024f01291294dfd2968
asharprayoga <87416872+asharprayoga@users.noreply.github.com>
2026-01-11 00:04:34 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,6 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./lib/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // kalau project kamu pakai src/
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
